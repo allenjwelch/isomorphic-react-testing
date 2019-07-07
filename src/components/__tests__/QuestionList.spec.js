d735c767d6f47be405f16d8592dad8cf360a9601
Allen Welch <allenjwelch@gmail.com>
2019-07-07 10:14:38 -0400

    initial commit

---
+++ src/components/__tests__/QuestionList.spec.js
@@ -0,0 +1,15 @@
+describe("The question list", () => {
+
+    // beforeEach(() => { console.log('before each') })
+    // beforeAll(() => { console.log('before all') })
+    // afterEach(() => { console.log('after each') })
+    // afterAll(()=> { console.log('after all') })
+
+    it("should display a list of items", () => {
+        expect(40+2).toEqual(42); 
+    })
+
+    it.skip("should be the meaning of life", () => {
+        expect(40 + 2).toEqual(43); 
+    })
+})
